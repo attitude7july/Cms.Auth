c5935376302f2196656e15fc5063729fd5a05a1a
shahidkochak@gmail.com <shahidkochak@gmail.com>
2021-04-22 23:23:11 +0800

    home page

--- Cms.Auth.IdentityProvider/Controllers/Home/HomeController.cs
+++ Cms.Auth.IdentityProvider/Controllers/Home/HomeController.cs
@@ -25,12 +25,11 @@ namespace Cms.Auth.IdentityProvider.Controllers
 
         public IActionResult Index()
         {
-           // if (_environment.IsDevelopment())
+            if (_environment.IsDevelopment())
             {
                 // only show in development
                 return View();
             }
-
             _logger.LogInformation("Homepage is disabled in production. Returning 404.");
             return NotFound();
         }
