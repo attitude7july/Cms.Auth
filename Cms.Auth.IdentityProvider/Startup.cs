d225658d9978720b3d13b9b217ca3f66c38f9bb1
shahidkochak@gmail.com <shahidkochak@gmail.com>
2021-04-24 17:54:57 +0800

    trying to add cookie policy

--- Cms.Auth.IdentityProvider/Startup.cs
+++ Cms.Auth.IdentityProvider/Startup.cs
@@ -1,6 +1,7 @@
 using Cms.Auth.IdentityProvider.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -63,7 +64,7 @@ namespace Cms.Auth.IdentityProvider
             app.UseCors("CorsPolicy");
             //use identity server
             app.UseIdentityServer();
-
+            app.UseCookiePolicy(new CookiePolicyOptions { MinimumSameSitePolicy = SameSiteMode.Strict });
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseRouting();
